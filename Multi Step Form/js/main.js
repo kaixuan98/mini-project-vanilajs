6b82b8baf5716454baa5411221f513ac8f2ea7dd
KaiXuan Chin <47827034+kaixuan98@users.noreply.github.com>
2022-12-26 16:31:34 +0800

    Finished Form Input

--- Multi Step Form/js/main.js
+++ Multi Step Form/js/main.js
@@ -13,26 +13,41 @@ let currentStep = formSteps.findIndex(step => {
 // whether is there an active class
 if (currentStep < 0 ){
     currentStep = 0; 
-    formSteps[currentStep].classList.add("form__page--active");
-    progressSteps[currentStep].classList.add("progress-bar__step--active");
-    progressSteps.forEach((step, index) => {
-        step.classList.toggle('progress-bar__step--inactive', index !== currentStep);
-    })
+    showCurrentStep();
+    showProgress();
 }
+
+// TODO: add click event on the progress bar to go back and forth
+
 
 // add click event on the next and prev button (event bubbling to multisetp form)
 multiStepForm.addEventListener("click" , (e) => {
     // when click the next button it will return a button
+    let incrementor;
     if (e.target.matches('[data-next]')){
-        currentStep += 1;
+        incrementor = 1;
     }else{
-        currentStep -= 1; 
+        incrementor = -1; 
+    }
+    
+    // this is to prevent form refreshed when click to fill in 
+    // since we are handling the click at the parent
+    if(incrementor === null) return;
+
+    // validate the form before move to the next step
+    // the values are required, clean(right formate)
+    const inputs = [...formSteps[currentStep].querySelectorAll('input')]
+    // validity is in combination with required in html tag
+    const allValid = inputs.every(input => input.reportValidity()); 
+    if(allValid){
+        currentStep += incrementor;
+        showCurrentStep();
+        showProgress();
     }
 
-    showCurrentStep();
-    showProgress();
 })
 
+//  helper functions
 function showCurrentStep(){
     formSteps.forEach( (step, index) => {
         step.classList.toggle("form__page--active", index === currentStep)
